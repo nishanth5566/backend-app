60aa2c42db096747c7bea2e12d4d0ce47e6af3b8
nishanth <nishanth>
2025-07-23 12:27:18 +0530

    step 4 admin router added

---
+++ Routes/admin.js
@@ -0,0 +1,33 @@
+const {Router} = require("express");
+const adminrouter = Router();
+
+
+adminrouter.post("/signup", function(req,res){
+    res.json({
+        message:"yoo signupppppppppp"
+    })
+})
+adminrouter.get("/sigin", function(req,res) {
+    res.json({
+        message:"yoo signinnnnnn"
+    })
+})
+adminrouter.post("/create", function(req,res) {
+    res.json({
+        message:"yoo signinnnnnn"
+    })
+})
+adminrouter.put("/courses", function(req,res) {
+    res.json({
+        message:"yoo signinnnnnn"
+    })
+})
+adminrouter.get ("/course/bulk", function(req,res) {
+    res.json({
+        message:"yoo signinnnnnn"
+    })
+})
+
+module.exports= {
+    adminrouter: adminrouter
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const{userRouter}= require("./Routes/user");
 const{courseRouter}= require("./Routes/courses");
+const{adminrouter}= require("./Routes/admin");
 const app = express();
 
 app.use("/api/v1/user", userRouter)
+app.use("/api/v1/admin", adminrouter)
 app.use("/api/v1/course", courseRouter)
 
 app.listen(3000);{
